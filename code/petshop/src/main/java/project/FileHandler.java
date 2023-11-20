df44f898f0d584acbf883e7dd095a426e00da9d0
Hussain.H <hussainharis@gmail.com>
2023-11-20 10:28:57 -0500

    added a few files

--- code/petshop/src/main/java/project/FileHandler.java
+++ code/petshop/src/main/java/project/FileHandler.java
@@ -1,5 +1,22 @@
 package project;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class FileHandler {
     
+    public List<Animal> loadAnimals (String Path){
+        try{
+            Path p = Paths.get(Path);
+            List<String> rows = Files.readAllLines(p);
+
+            for(String row : rows){
+                String[] columns
+            }
+
+        }
+    }
+
 }
