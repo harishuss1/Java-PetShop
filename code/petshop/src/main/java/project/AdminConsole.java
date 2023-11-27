3bb65e20e5a6fa7099f95a9afff987153ebad78a
wuKevin <wukevin1200@gmail.com>
2023-11-27 15:03:12 -0500

    few changes

--- code/petshop/src/main/java/project/AdminConsole.java
+++ code/petshop/src/main/java/project/AdminConsole.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class AdminConsole extends Console {
     final static Scanner scanner = new Scanner(System.in);
-    
+
     User user = new User(null, null, 0);
 
     protected InventoryManager inventoryManager;
@@ -23,22 +23,33 @@ public class AdminConsole extends Console {
         System.out.println("Menu:");
         System.out.println("1. Manage inventory");
         System.out.println("2. Manage promo code");
-        System.out.println("3. Exit");
+        System.out.println("3. Manage admins");
+        System.out.println("4. Exit");
         spacing();
 
         int choice = getUserChoice();
-        scanner.nextLine();
+        // scanner.nextLine();
         switch ((choice)) {
             case 1:
+                spacing();
                 ManageInventory();
                 displayMainMenu();
                 break;
             case 2:
+                spacing();
                 ManagePromoCodes();
                 displayMainMenu();
                 break;
-            
+            case 3:
+                spacing();
+                ManageAdmins();
+                break;
+            case 4:
+                spacing();
+                System.out.println("Exiting the virtual petShop. See you next time!");
+                break;
             default:
+                spacing();
                 System.out.println("Invalid choice. Please try again.");
                 displayMainMenu();
                 break;
@@ -56,22 +67,28 @@ public class AdminConsole extends Console {
         int choice = getUserChoice();
         switch (choice) {
             case 1:
+                spacing();
                 addAnimal();
                 break;
             case 2:
+                spacing();
                 updateAnimal();
                 break;
             case 3:
+                spacing();
                 removeAnimal();
                 break;
             case 4:
-                InventoryManager inventoryManager =  new InventoryManager();
+                spacing();
+                InventoryManager inventoryManager = new InventoryManager();
                 inventoryManager.viewAllAnimals();
                 break;
             case 5:
+                spacing();
                 displayMainMenu();
                 break;
             default:
+                spacing();
                 System.out.println("Invalid choice. Please try again.");
                 ManageInventory();
                 break;
@@ -89,19 +106,56 @@ public class AdminConsole extends Console {
         int choice = getUserChoice();
         switch (choice) {
             case 1:
-                
+                spacing();
+                // add promo
                 break;
             case 2:
-
+                spacing();
+                // update promo
                 break;
             case 3:
-
+                // remove promo
                 break;
             case 4:
-
+                // remove promo
                 break;
             default:
-            
+                spacing();
+                System.out.println("Invalid choice. Please try again.");
+                ManagePromoCodes();
+                break;
+        }
+    }
+
+    public void ManageAdmins() {
+        System.out.println("Managing Admins:");
+        System.out.println("1. Add an admin");
+        System.out.println("2. Update an admin");
+        System.out.println("3. Remove an admin");
+        System.out.println("4. Return to Menu");
+
+        int choice = getUserChoice();
+        switch (choice) {
+            case 1:
+                spacing();
+                // add admin
+                break;
+            case 2:
+                spacing();
+                // update admin
+                break;
+            case 3:
+                spacing();
+                // remove admin
+                break;
+            case 4:
+                spacing();
+                displayMainMenu();
+                break;
+            default:
+                spacing();
+                System.out.println("Invalid choice. Please try again.");
+                ManageAdmins();
                 break;
         }
     }
@@ -115,51 +169,50 @@ public class AdminConsole extends Console {
         int age = scanner.nextInt();
         System.out.println("Enter new price:");
         double price = scanner.nextDouble();
-        scanner.nextLine(); 
-    
+        scanner.nextLine();
+
         return new Animal(name, species, age, price);
     }
-    
+
     private void addAnimal() {
         Animal addedAnimal = getAnimalDetailsFromUser();
 
         inventoryManager.addAnimal(addedAnimal);
         System.out.println("Animal added successfully!");
-    
+
         spacing();
         ManageInventory();
     }
-    
+
     private void updateAnimal() {
         System.out.println("Updating an Animal:");
         System.out.println("Enter the name of the animal to update:");
         String oldName = scanner.nextLine();
-    
+
         Animal newAnimal = getAnimalDetailsFromUser(); // Gather updated information
         inventoryManager.updateAnimal(oldName, newAnimal);
-    
+
         System.out.println("Animal updated successfully!");
-    
+
         spacing();
         ManageInventory();
     }
-    
-    
+
     private void removeAnimal() {
         System.out.println("Removing an Animal:");
         System.out.println("Enter the name of the animal to remove:");
         String name = scanner.nextLine();
-    
+
         boolean removed = inventoryManager.removeAnimal(name);
-    
+
         if (removed) {
             System.out.println("Animal removed successfully!");
         } else {
             System.out.println("Animal not found in inventory.");
         }
-    
+
         spacing();
         ManageInventory();
     }
-    
+
 }
