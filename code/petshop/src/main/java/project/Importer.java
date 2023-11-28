2022ad2f267aac853c269e4e398b996a64e9e44e
wuKevin <wukevin1200@gmail.com>
2023-11-28 15:13:54 -0500

    Promo code menu fully functional

--- code/petshop/src/main/java/project/Importer.java
+++ code/petshop/src/main/java/project/Importer.java
@@ -3,12 +3,22 @@ package project;
 import java.io.IOException;
 import java.util.List;
 
-public interface IPromoCodeImporter {
+public interface Importer {
 
+    // ANIMAL
+    List<Animal> loadAnimals() throws Exception;
+
+    // PROMOCODE
     List<PromoCode> loadPromoCodes(String path) throws IOException;
 
     void writePromoCode(String code, double value, String path) throws IOException;
 
+    void updatePromoCode(String code, int newValue, String path) throws IOException;
+
     void deletePromoCode(String code, String path) throws IOException;
+
+    void prntPromo(String path) throws IOException;
+
+    // ADMIN
 
 }
