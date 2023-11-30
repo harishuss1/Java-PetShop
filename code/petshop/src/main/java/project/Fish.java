063dc146cd196310f31f76938d6307da368eb33a
Haris Hussain <hussainharis170@gmail.com>
2023-11-29 21:43:48 -0500

    Changed all animals currently working on it

---
+++ code/petshop/src/main/java/project/Fish.java
@@ -0,0 +1,24 @@
+package project;
+
+public class Fish extends Animal {
+
+    private String color;
+
+    public Fish(String name,String species,int age, double price, String color) {
+        super(name, species, age, price);
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "\nColor: " + color;
+    }
+}
